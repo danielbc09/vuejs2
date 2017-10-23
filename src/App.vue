095062c814378ac10f8eb51f3aed82d3c1b231d7
Your Name <you@example.com>
2017-10-22 21:27:30 -0500

    Creating a Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <h1>Hello world</h1>
-  </div>
+    <app-server-status></app-server-status>
 </template>
 
 <script>
 export default {
+    data:function () {
+        return{
+            status: 'Critical'
+        }
+    }
 }
 </script>
 
